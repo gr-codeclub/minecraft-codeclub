ec8adb94be06b17e044e259f02a788a2e760df6b
gr-codeclub12 <gr-codeclub@proton.me>
2023-05-16 16:59:11 +0100

    I made a block!

---
+++ catcosmonaut/src/main/java/net/mcreator/catcosmonaut/init/CatcosmonautModBlocks.java
@@ -0,0 +1,19 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.catcosmonaut.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.level.block.Block;
+
+import net.mcreator.catcosmonaut.block.SafaanaumrahmanBlock;
+import net.mcreator.catcosmonaut.CatcosmonautMod;
+
+public class CatcosmonautModBlocks {
+	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, CatcosmonautMod.MODID);
+	public static final RegistryObject<Block> SAFAANAUMRAHMAN = REGISTRY.register("safaanaumrahman", () -> new SafaanaumrahmanBlock());
+}
