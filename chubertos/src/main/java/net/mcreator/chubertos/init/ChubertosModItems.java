c14a6775c12065e2ed9255c69ea213de392c0edf
codeclub20235 <134068729+codeclub20235@users.noreply.github.com>
2023-06-06 16:59:49 +0100

    Imade a wand

--- chubertos/src/main/java/net/mcreator/chubertos/init/ChubertosModItems.java
+++ chubertos/src/main/java/net/mcreator/chubertos/init/ChubertosModItems.java
@@ -13,11 +13,17 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.chubertos.item.DexterItem;
 import net.mcreator.chubertos.ChubertosMod;
 
 public class ChubertosModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, ChubertosMod.MODID);
+	public static final RegistryObject<Item> DEXTER_HELMET = REGISTRY.register("dexter_helmet", () -> new DexterItem.Helmet());
+	public static final RegistryObject<Item> DEXTER_CHESTPLATE = REGISTRY.register("dexter_chestplate", () -> new DexterItem.Chestplate());
+	public static final RegistryObject<Item> DEXTER_LEGGINGS = REGISTRY.register("dexter_leggings", () -> new DexterItem.Leggings());
+	public static final RegistryObject<Item> DEXTER_BOOTS = REGISTRY.register("dexter_boots", () -> new DexterItem.Boots());
 	public static final RegistryObject<Item> KHALEDBLOCK = block(ChubertosModBlocks.KHALEDBLOCK, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> DEXTER_3 = REGISTRY.register("dexter_3", () -> new Dexter3Item());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
---
+++ chubertos/src/main/java/net/mcreator/chubertos/item/Dexter3Item.java
@@ -0,0 +1,12 @@
+
+package net.mcreator.chubertos.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+
+public class Dexter3Item extends Item {
+	public Dexter3Item() {
+		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON));
+	}
+}
