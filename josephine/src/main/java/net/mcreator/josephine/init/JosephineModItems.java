2105c1799eea9b132718c4db5e2f8c6d5ac78b9c
codeclub202310 <134087208+codeclub202310@users.noreply.github.com>
2023-06-06 16:50:30 +0100

    i made a cake

--- josephine/src/main/java/net/mcreator/josephine/init/JosephineModItems.java
+++ josephine/src/main/java/net/mcreator/josephine/init/JosephineModItems.java
@@ -13,11 +13,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.josephine.item.CakeItem;
 import net.mcreator.josephine.JosephineMod;
 
 public class JosephineModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, JosephineMod.MODID);
 	public static final RegistryObject<Item> JOSEPHINE = block(JosephineModBlocks.JOSEPHINE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> CAKE = REGISTRY.register("cake", () -> new CakeItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
