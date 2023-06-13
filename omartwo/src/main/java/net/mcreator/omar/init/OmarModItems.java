aea4212c4ccaa83cc38b1cf40e23806ddb5ff79a
codeclub202311 <134087385+codeclub202311@users.noreply.github.com>
2023-06-13 16:56:03 +0100

    cake

--- omartwo/src/main/java/net/mcreator/omar/init/OmarModItems.java
+++ omartwo/src/main/java/net/mcreator/omar/init/OmarModItems.java
@@ -13,10 +13,12 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.omar.item.CakeItem;
 import net.mcreator.omar.OmarMod;
 
 public class OmarModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, OmarMod.MODID);
+	public static final RegistryObject<Item> CAKE = REGISTRY.register("cake", () -> new CakeItem());
 	public static final RegistryObject<Item> OMAR = block(OmarModBlocks.OMAR, CreativeModeTab.TAB_BUILDING_BLOCKS);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
