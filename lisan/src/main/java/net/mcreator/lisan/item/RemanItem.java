18876a6342213cf11e86f355fdaa715cdcd4d070
codeclub202313 <134088690+codeclub202313@users.noreply.github.com>
2023-06-13 16:57:18 +0100

    food

--- lisan/src/main/java/net/mcreator/lisan/item/RemanItem.java
+++ lisan/src/main/java/net/mcreator/lisan/item/RemanItem.java
@@ -1,12 +1,27 @@
 
 package net.mcreator.lisan.item;
 
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.food.FoodProperties;
 
 public class RemanItem extends Item {
 	public RemanItem() {
-		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(4).saturationMod(0.3f)
+
+				.build()));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.NONE;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 1;
 	}
 }
