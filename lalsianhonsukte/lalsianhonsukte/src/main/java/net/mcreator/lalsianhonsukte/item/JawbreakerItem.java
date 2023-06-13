1925c2215259677787beedbed63a9d13545115a3
codeclub20234 <134068441+codeclub20234@users.noreply.github.com>
2023-06-13 16:51:38 +0100

    I ate my food.

--- lalsianhonsukte/lalsianhonsukte/src/main/java/net/mcreator/lalsianhonsukte/item/JawbreakerItem.java
+++ lalsianhonsukte/lalsianhonsukte/src/main/java/net/mcreator/lalsianhonsukte/item/JawbreakerItem.java
@@ -1,12 +1,27 @@
 
 package net.mcreator.lalsianhonsukte.item;
 
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.food.FoodProperties;
 
 public class JawbreakerItem extends Item {
 	public JawbreakerItem() {
-		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(4).saturationMod(0.3f).alwaysEat()
+
+				.build()));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.NONE;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 1;
 	}
 }
