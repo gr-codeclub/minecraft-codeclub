961bc3fa575faeeb22ee3c8176415a01ce65eb83
codeclub202316 <134312470+codeclub202316@users.noreply.github.com>
2023-06-13 16:55:51 +0100

    FOOD

--- ronaldo/src/main/java/net/mcreator/ronaldo/item/CakeItem.java
+++ ronaldo/src/main/java/net/mcreator/ronaldo/item/CakeItem.java
@@ -1,12 +1,25 @@
 
 package net.mcreator.ronaldo.item;
 
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.food.FoodProperties;
 
 public class CakeItem extends Item {
 	public CakeItem() {
-		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(4).saturationMod(0.3f).alwaysEat().meat().build()));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.NONE;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 1;
 	}
 }
