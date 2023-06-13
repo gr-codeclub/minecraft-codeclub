46a90f1460c1205ae73e1d85a4054f8eb9f490b8
codeclub202315 <134103654+codeclub202315@users.noreply.github.com>
2023-06-13 16:55:09 +0100

    eating

--- jamila/src/main/java/net/mcreator/jamilafarah/init/JamilafarahModItems.java
+++ jamila/src/main/java/net/mcreator/jamilafarah/init/JamilafarahModItems.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.jamilafarah.item.PizzaTwoItem;
 import net.mcreator.jamilafarah.item.PizzaItem;
 import net.mcreator.jamilafarah.JamilafarahMod;
 
@@ -20,6 +21,7 @@ public class JamilafarahModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, JamilafarahMod.MODID);
 	public static final RegistryObject<Item> PIZZA = REGISTRY.register("pizza", () -> new PizzaItem());
 	public static final RegistryObject<Item> JAMILA = block(JamilafarahModBlocks.JAMILA, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> PIZZA_TWO = REGISTRY.register("pizza_two", () -> new PizzaTwoItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
---
+++ jamila/src/main/java/net/mcreator/jamilafarah/item/PizzaTwoItem.java
@@ -0,0 +1,27 @@
+
+package net.mcreator.jamilafarah.item;
+
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.food.FoodProperties;
+
+public class PizzaTwoItem extends Item {
+	public PizzaTwoItem() {
+		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(4).saturationMod(0.3f)
+
+				.build()));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.NONE;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 1;
+	}
+}
