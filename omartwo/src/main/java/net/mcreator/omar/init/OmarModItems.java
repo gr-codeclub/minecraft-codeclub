3c749855197721940255421fde6636cd2e801aa5
codeclub202311 <134087385+codeclub202311@users.noreply.github.com>
2023-06-20 16:57:54 +0100

    wand

--- omartwo/src/main/java/net/mcreator/omar/init/OmarModItems.java
+++ omartwo/src/main/java/net/mcreator/omar/init/OmarModItems.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.omar.item.WandtoolItem;
 import net.mcreator.omar.item.CakeItem;
 import net.mcreator.omar.OmarMod;
 
@@ -20,6 +21,7 @@ public class OmarModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, OmarMod.MODID);
 	public static final RegistryObject<Item> CAKE = REGISTRY.register("cake", () -> new CakeItem());
 	public static final RegistryObject<Item> OMAR = block(OmarModBlocks.OMAR, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> WANDTOOL = REGISTRY.register("wandtool", () -> new WandtoolItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
