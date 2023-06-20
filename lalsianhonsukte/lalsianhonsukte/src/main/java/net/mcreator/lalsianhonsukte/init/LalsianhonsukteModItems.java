0a5f0c0bd962a7cdf5a8e0def0100ddb08dff70a
codeclub20234 <134068441+codeclub20234@users.noreply.github.com>
2023-06-20 16:58:30 +0100

    I made a wand

--- lalsianhonsukte/lalsianhonsukte/src/main/java/net/mcreator/lalsianhonsukte/init/LalsianhonsukteModItems.java
+++ lalsianhonsukte/lalsianhonsukte/src/main/java/net/mcreator/lalsianhonsukte/init/LalsianhonsukteModItems.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.lalsianhonsukte.item.WandItem;
 import net.mcreator.lalsianhonsukte.item.JawbreakerItem;
 import net.mcreator.lalsianhonsukte.LalsianhonsukteMod;
 
@@ -20,6 +21,7 @@ public class LalsianhonsukteModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, LalsianhonsukteMod.MODID);
 	public static final RegistryObject<Item> JAWBREAKER = REGISTRY.register("jawbreaker", () -> new JawbreakerItem());
 	public static final RegistryObject<Item> LALSIAN = block(LalsianhonsukteModBlocks.LALSIAN, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> WAND = REGISTRY.register("wand", () -> new WandItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
