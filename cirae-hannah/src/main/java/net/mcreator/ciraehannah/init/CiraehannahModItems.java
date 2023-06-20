1ef26cad9fbdb447d10d6aa637994ad59e5338e1
codeclub20236 <134069199+codeclub20236@users.noreply.github.com>
2023-06-20 16:46:08 +0100

    i nearly finisht a house
    i made a wand

--- cirae-hannah/src/main/java/net/mcreator/ciraehannah/init/CiraehannahModItems.java
+++ cirae-hannah/src/main/java/net/mcreator/ciraehannah/init/CiraehannahModItems.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.ciraehannah.item.WandItem;
 import net.mcreator.ciraehannah.item.NoodlesItem;
 import net.mcreator.ciraehannah.CiraehannahMod;
 
@@ -20,6 +21,7 @@ public class CiraehannahModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, CiraehannahMod.MODID);
 	public static final RegistryObject<Item> NOODLES = REGISTRY.register("noodles", () -> new NoodlesItem());
 	public static final RegistryObject<Item> CIRAE = block(CiraehannahModBlocks.CIRAE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> WAND = REGISTRY.register("wand", () -> new WandItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
